49c25e328991c8fa89120abf3bc112f3013d6f0a
Sezarfen <110563468+sezarfen@users.noreply.github.com>
2024-02-03 20:19:51 +0000

    Some changes and improvements on the files that effected

--- new_codes/test.cpp
+++ new_codes/test.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+
+/*
 // This cpp file is to test "new" keyword, and scope.
 
 class A
@@ -42,4 +44,25 @@ int main()
     // if we won't say delete a; it will cause a 8 byte (4 + 4) leak coming from A class
     delete a;
     return (EXIT_SUCCESS);
+}
+*/
+
+#include <fstream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+        return (std::cout << "Please use this format ./a.out [file to read] [file to write]" << std::endl, 1);
+
+    std::ofstream fileToWrite;
+    std::ifstream fileToRead;
+    std::string temp;
+
+    fileToRead.open(argv[1]);
+    fileToWrite.open(argv[2]);
+    
+    while (fileToRead >> temp && fileToWrite << temp);
+
+    return (0);
 }
