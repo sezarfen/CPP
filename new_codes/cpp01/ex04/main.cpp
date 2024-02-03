560d3cf101f2780a1eafb59e8c0e46499d8a961e
Sezarfen <110563468+sezarfen@users.noreply.github.com>
2024-02-03 15:58:26 +0000

    Updates

--- new_codes/cpp01/ex04/main.cpp
+++ new_codes/cpp01/ex04/main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+#include <fstream>
+
 
 int main(int argc, char *argv[])
 {
+    /*
     if (argc != 4)
     {
-        std::cout << "Please use this format : " << " ./program [file_name] [string 1] [string 2]"
+        std::cout << "Please use this format : " << " ./program [file_name] [string 1] [string 2]" << std::endl;
     }
+    */
+    // TODO: Error handling to check arguments giving correctly or not
 
-    return ();
+    // File progress
+    std::ofstream file;
+
+    file.open(argv[1]);
+    file << "Something written on this file" << std::endl;
+    file.close();
+
+    return (0);
 }
