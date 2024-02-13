8bbd517f35eaf20ddd3a4bbc04a63fafd466cc50
Sezarfen <110563468+sezarfen@users.noreply.github.com>
2024-02-13 17:08:42 +0000

    Bismillahirrahmanirrahim, new file

---
+++ new_codes/cpp02/ex03/bsp.cpp
@@ -0,0 +1,12 @@
+#include "Point.hpp"
+
+/**
+ * • a, b, c: The vertices of our beloved triangle
+ * • point: The point to check.
+ * • Returns: True if the point is inside the triangle. False otherwise.
+Thus, if the point is a vertex or on edge, it will return False.
+*/
+bool bsp( Point const a, Point const b, Point const c, Point const point)
+{
+    
+}
