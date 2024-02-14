84585a37791aadd1979b5215bbf8ab3a610d9557
Sezarfen <110563468+sezarfen@users.noreply.github.com>
2024-02-14 13:06:11 +0000

    Bismillahirrahmanirrahim updates on ex03

--- new_codes/cpp02/ex03/main.cpp
+++ new_codes/cpp02/ex03/main.cpp
@@ -2,11 +2,11 @@
 
 int main( void ) 
 {
-    Point p1(0, 5);
-    Point p2(3, 0);
-    Point p3(-3, -3);
+    Point p1(0, 5); // Point at (0, 5);
+    Point p2(3, 0); // Point at (3, 0);
+    Point p3(-3, -3); // Point at (-3, -3)
 
-    Point myPoint(-1, 0);
+    Point myPoint(4, 0); // Point at (4 ,0)
 
     if (bsp(p1, p2, p3, myPoint))
         std::cout << "Your point inside of the triangle" << std::endl;
